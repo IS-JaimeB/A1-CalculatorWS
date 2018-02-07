66604061128889f0a5015b948b7059a1b4c5aa70
Jaime <Jaime@10.230.170.18>
2018-02-07 12:08:23 +0000

    first commit

---
+++ src/java/calc/calc.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calc;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author entrar
+ */
+@WebService(serviceName = "calc")
+@Stateless()
+public class calc {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "sum")
+    public int sum(@WebParam(name = "n1") int n1, @WebParam(name = "n2") int n2) {
+        int res=n1+n2;
+        return res;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "rest")
+    public int rest(@WebParam(name = "n1") int n1, @WebParam(name = "n2") int n2) {
+        int res = n1-n2;
+        return res;
+    }
+}
